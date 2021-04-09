ae9082c5f1162f8800dd7fb9e0973714e8275e0b
Eric Coulter <jecoulte@iu.edu>
2021-04-09 19:30:54 +0000

    changed to Singularity
    and added the charmplusplus definition back to prod-env
    
    Signed-off-by: Eric Coulter <jecoulte@iu.edu>

--- namd2-14.nix
+++ namd2-14.nix
@@ -1,5 +1,26 @@
 with import <nixpkgs> {};
 let
+  charmplusplus-singlenode = stdenv.mkDerivation {
+    name = "charmplusplus-singlenode";
+    
+    src = fetchurl {
+      url = http://charm.cs.illinois.edu/distrib/charm-6.10.2.tar.gz;
+      sha256 = "7abb4cace8aebdfbb8006eac03eb766897c009cfb919da0d0a33f74c3b4e6deb";
+    };
+#    phases = "unpackPhase buildPhase"; # I suspect this was actually stopping the Unpack phase from happening... hence the schlepping around inside InstallPhase
+    
+    buildPhase = ''
+#we are building for single-node namd...
+      ./build charm++ multicore-linux-x86_64 gcc --with-production
+    '';
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r ./* $out
+    '';
+
+#It seems like a good idea to run some sort of cleanup now, but I don't know what can be safely removed yet!
+  };
   namd2-14 = stdenv.mkDerivation {
     name = "namd2-14";
 
